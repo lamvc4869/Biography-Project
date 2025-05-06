489885b4f5d6a79b4dc7d1ce392ba0a6c448d208
LamVC4869 <lamcity4869@gmail.com>
2025-05-06 23:38:25 +0700

    update project

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello! Admin')
+const sections = document.querySelectorAll(".section");
+const navLinks = document.querySelectorAll(".navbar-right a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollY >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
